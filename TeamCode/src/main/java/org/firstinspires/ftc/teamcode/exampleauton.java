ad1923197f1e41f7319883b139e7240e6e7e361d
Aarushboss <aarush.wins.money@gmail.com>
2025-07-26 09:38:23 -0400

    example auto and teleop for summer camp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exampleauton.java
@@ -0,0 +1,106 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous (name="example auto", group="Linear OpMode")
+@Disabled
+public class exampleauton extends LinearOpMode {
+    DcMotor FL;
+    DcMotor FR;
+    DcMotor BL;
+    DcMotor BR;
+
+    double CPR = 537.7;
+    int armCPR = 8192;
+
+
+    double diameter = 9.6; // Replace with your wheel/spool's diameter
+    double circumference = Math.PI * diameter;
+    int drivetrainconvert = 17;
+    int armconvert = -23;
+
+
+    public void resetencoders() {
+
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void drive(int desiredPosition) {
+        FL.setTargetPosition(desiredPosition*drivetrainconvert); // Tells the motor that the position it should go to is desiredPosition
+        FR.setTargetPosition(desiredPosition*drivetrainconvert); // Tells the motor that the position it should go to is desiredPosition
+        BL.setTargetPosition(desiredPosition*drivetrainconvert); // Tells the motor that the position it should go to is desiredPosition
+        BR.setTargetPosition(desiredPosition*drivetrainconvert); // Tells the motor that the position it should go to is desiredPosition
+
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (FL.isBusy() || FR.isBusy() || BR.isBusy() || BL.isBusy()){
+            FL.setPower(0.5);
+            FR.setPower(0.5);
+            BL.setPower(0.5);
+            BR.setPower(0.5);
+        }
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+    }
+
+
+    public void runOpMode() {
+        FL = hardwareMap.get(DcMotor.class, "motor");
+        FR = hardwareMap.get(DcMotor.class, "motor");
+        BL = hardwareMap.get(DcMotor.class, "motor");
+        BR = hardwareMap.get(DcMotor.class, "motor");
+
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        BR.setDirection(DcMotor.Direction.REVERSE);
+
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+
+
+        waitForStart();
+
+
+        if (opModeIsActive()) {
+
+            drive(30);
+            resetencoders();
+            drive(-30);
+            resetencoders();
+
+
+        }
+
+
+    }
+
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exampleteleop.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@TeleOp
+@Disabled
+public class exampleteleop extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+
+
+        DcMotor motor;
+        motor = hardwareMap.get(DcMotor.class, "motor");
+
+
+
+        motor.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+
+
+        while (opModeIsActive()) {
+            //teleop goes on here  -- where you tell the robot exactly what to do during the program
+
+            if (gamepad1.a) {
+                motor.setPower(1);
+            } else if (gamepad1.b) {
+                motor.setPower(-1);
+            } else {
+                motor.setPower(0);
+            }
+
+
+        }
+    }
+}
